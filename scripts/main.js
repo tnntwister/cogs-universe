1014d3a361a8fddde762bd9064077a8d24bf42b0
theripper93 <tsplab@gmail.com>
2023-06-18 18:19:39 +0200

    a

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 import {initConfig} from "./config.js";
 import { registerSettings } from "./settings.js";
 
+export const MODULE_ID = "module-id";
+
 Hooks.on("init", () => {
     initConfig();
     registerSettings();
