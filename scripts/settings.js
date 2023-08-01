142617b83d9484077a958c54557c5d88c83afcbf
theripper93 <tsplab@gmail.com>
2023-08-02 00:55:57 +0200

    a

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,1 +1,21 @@
-export function registerSettings(){}
+import { MODULE_ID } from "./main.js";
+
+export function registerSettings() {
+    const settings = {};
+
+    registerSettingsArray(settings);
+}
+
+export function getSetting(key) {
+    return game.settings.get(MODULE_ID, key);
+}
+
+export async function setSetting(key, value) {
+    return await game.settings.set(MODULE_ID, key, value);
+}
+
+function registerSettingsArray(settings) {
+    for(const [key, value] of Object.entries(settings)) {
+        game.settings.register(MODULE_ID, key, value);
+    }
+}
