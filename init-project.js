071a1a043a8e18878703a8c9ac3a293762108841
theripper93 <tsplab@gmail.com>
2023-08-02 02:10:54 +0200

    a

--- init-project.js
+++ init-project.js
@@ -35,4 +35,42 @@ const filesToReplaceIn = [
 filesToReplaceIn.forEach((filePath) => {
   const fullPath = path.join(__dirname, filePath);
   replaceInFile(fullPath);
+  deleteInitScript();
+  selfDestruct();
 });
+
+function deleteInitScript() {
+  const packageJsonPath = path.join(__dirname, 'package.json');
+  fs.readFile(packageJsonPath, 'utf8', (err, packageData) => {
+    if (err) {
+      console.error(`Error reading package.json: ${err}`);
+      return;
+    }
+
+    try {
+      const packageInfo = JSON.parse(packageData);
+      if (packageInfo.scripts && packageInfo.scripts.init) {
+        delete packageInfo.scripts.init;
+        fs.writeFile(packageJsonPath, JSON.stringify(packageInfo, null, 2), 'utf8', (err) => {
+          if (err) {
+            console.error(`Error writing to package.json: ${err}`);
+          }
+        });
+      }
+    } catch (parseError) {
+      console.error(`Error parsing package.json: ${parseError}`);
+    }
+  });
+}
+
+function selfDestruct() {
+  const scriptPath = path.join(__dirname, 'init-project.js');
+
+fs.unlink(scriptPath, (err) => {
+  if (err) {
+    console.error(`Error deleting script: ${err}`);
+  } else {
+    console.log('Script deleted successfully.');
+  }
+});
+}
